3c251291abfb30b2d8628e56ae0829a6f5c8041f
rkrug5 <phishermankrug@hotmail.com>
2018-05-05 09:38:45 -0700

    test

--- models/burger.js
+++ models/burger.js
@@ -3,3 +3,7 @@
 
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 // Export at the end of the burger.js file.
+//--------------------------------------------------------------------------------------------------------
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
